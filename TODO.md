89083f5a167a8a69d8e0e344319cbae9b8f2ef7f
pzia <paparazzia@gmail.com>
2015-04-26 21:59:42 +0200

    working poc with jtable
    NB : jtable is CRUD, not pure httpREST

--- TODO.md
+++ TODO.md
@@ -2,6 +2,13 @@
 
 ##Définir une api rest
 
+###Quelques sources
+* l'api twitter : https://dev.twitter.com/rest/public
+* une belle synthèse opérationnelle : http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api
+* l'api stripe : https://stripe.com/docs/api#intro
+* une autre synthèse de "best practice" : https://blog.apigee.com/detail/restful_api_design_can_your_api_give_developers_just_the_information
+* la RFC pour les template d'url : https://tools.ietf.org/html/rfc6570
+
 ###Project
 * GET /projects/ #Liste des projets
 * POST /projects/ #Insert project(s)
@@ -66,16 +73,27 @@
 - sqlite pour la db
 - datatables pour l'ihm
 
-##Définir la test suite
+##Définir une test suite
+* ce serait bien...
+* 
 
-##Utiliser bottle en python
-* Faire un test rapide OK
+##Développer l'api en python
+* Utilisation de bottle : http://bottlepy.org/docs/dev/index.html
+* Faire un test rapide : OK dans src/misc/testbottle.py
+* Développer une API complète pour une des tables
+* ? Rendre générique le code :
+  * Découvrir le modèle de donnée dynamiquement
+  * Construire l'api dynamiquement
+  * Worth reading : http://fr.slideshare.net/jeffknupp/building-automated-rest-apis-with-python
+
+##Implémenter l'utilisation de l'API
+* Basiquement, on est face à une IHM d'administration de tables : utilisons datables : https://datatables.net/
+*
 
 ##Restitution graphique
-
 * Essayer Google Chart : https://google-developers.appspot.com/chart/interactive/docs/gallery/bubblechart
   * Ce qui se traduirait par une page html avec un peu de javascript qui charge les données (Api ?) et les restitue
 
 ##Gérer l'authentification pour l'api
-
+* REST & Authentification : http://broadcast.oreilly.com/2009/12/principles-for-standardized-rest-authentication.html
 
--- src/rest/app.py
+++ src/rest/app.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Service Rest pour chronodatas"""
+"""Service web pour chronodatas (jtable api)"""
 
 #imports
 import json
 import bottle
 import bottle_sqlite
 import sqlite3
+import webbrowser
 
 #globals
 dbpath = '../datas/sqlite.db'
+apppath = '../app'
+fieldsRef = {
+    'groups' : ['name', 'date_start', 'date_end'],
+    'projects' : ['name', 'id_group', "date_impact"],
+    'locations' : ['name'],
+    'impacts' : ['id_project', 'id_location', 'type', 'real', 'felt'],
+}
 
 #bottle app
 app = bottle.Bottle()
 sqlite_plugin = bottle_sqlite.SQLitePlugin(dbfile=dbpath)
+
+def tableKey(table):
+    return "id_"+table[:-1]
+
+@app.route('/app/<filename:path>')
+def send_static(filename):
+    return bottle.static_file(filename, root=apppath)
 
 @app.route('/')
 def documentation():
@@ -31,14 +46,81 @@ def documentation():
         ret += "<b>%s</b>&nbsp;<i><a href=\"%s\">%s</a></i>&nbsp;\"%s\"<br/>" % (l['method'], l['rule'], l['rule'], l['doc'])
     return ret
 
-@app.get('/groups', apply=[sqlite_plugin])
-def groups(db):
-    """Liste les groupes de projets"""
+@app.post('/<table:re:[a-z]+>/list', apply=[sqlite_plugin])
+def listing(table, db):
+    """Liste les lignes d'une table"""
     rows = []
-    for row in db.execute('SELECT * FROM groups'):
+    for row in db.execute("SELECT * FROM %s" % table):
         rows.append(dict(row))
-    return json.dumps({'groups': rows})
-    
+    res = {}
+    res['Result'] = 'OK'
+    res['Records'] = rows        
+    return json.dumps(res)
+
+@app.post('/<table:re:[a-z]+>/options/<option:re:[a-z]+>', apply=[sqlite_plugin])
+def options(table, option, db):
+    """Liste les lignes d'une table"""
+    rows = []
+    for row in db.execute("SELECT * FROM %s" % table):
+        r = dict(row)
+        rows.append({'DisplayText' : r[option], 'Value' : r[tableKey(table)]})
+    res = {}
+    res['Result'] = 'OK'
+    res['Options'] = rows        
+    return json.dumps(res)
+
+
+@app.post('/<table:re:[a-z]+>/<parent:re:[a-z]+>/<ID:int>', apply=[sqlite_plugin])
+def projects(table, parent, ID, db):
+    """Liste les lignes d'une table relative à un parent"""
+    rows = []
+    for row in db.execute("SELECT * FROM %s WHERE %s = %d" % (table, tableKey(parent), int(ID))):
+        rows.append(dict(row))
+    res = {}
+    res['Result'] = 'OK'
+    res['Records'] = rows        
+    return json.dumps(res)
+
+@app.post('/<table:re:[a-z]+>/create', apply=[sqlite_plugin])
+def create(table, db):
+    """crée un enregistrement"""
+    fields = fieldsRef[table]
+    vals = []
+    for field in fields :
+        vals.append(bottle.request.forms.get(field))
+    sql = "INSERT INTO %s (" % table
+    sql += ",".join(fields)
+    sql += ") VALUES ("
+    sql += "'"+"', '".join(vals)+"'"
+    sql += ");"
+    db.execute(sql)
+    row = db.execute("SELECT * FROM %s WHERE %s = last_insert_rowid()" % (table, tableKey(table))).fetchone();
+    res = {
+        'Result' : 'OK',
+        'Record' : dict(row)
+    }
+    return json.dumps(res)
+
+@app.post('/<table:re:[a-z]+>/update', apply=[sqlite_plugin])
+def update(table, db):
+    """mis à jour un enregistrement"""
+    fields = fieldsRef[table]
+    vals = []
+    for field in fields :
+        vals.append("%s = '%s'" % (field, bottle.request.forms.get(field)))
+    sql = "UPDATE %s SET " % table
+    sql += ", ".join(vals)
+    sql += " WHERE %s = %d;" % (tableKey(table), int(bottle.request.forms.get(tableKey(table))))
+    db.execute(sql)
+    row = db.execute("SELECT * FROM %s WHERE %s = last_insert_rowid()" % (table, tableKey(table))).fetchone();
+    return json.dumps({'Result' : 'OK'})
+
+@app.post('/<table:re:[a-z]+>/delete', apply=[sqlite_plugin])
+def delete_group(table, db):
+    """Efface"""
+    row = db.execute('DELETE FROM %s WHERE %s = %d' % (table, tableKey(table), int(bottle.request.forms.get(tableKey(table)))))
+    return json.dumps({'Result' : 'OK'})
+
 @app.route('/dump', apply=[sqlite_plugin])
 def dump(db):
     """Renvoi l'export complet des données"""
@@ -47,5 +129,6 @@ def dump(db):
         ret += '%s<br/>' % line
     return ret
 
+#webbrowser.open("http://127.0.0.1:8080/app/index.html")
 app.run(reloader=True)
 
