31bff4d82faf1239d873d7ffea79884cb79ddf96
pzia <paparazzia@gmail.com>
2015-04-26 22:02:09 +0200

    Update database

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -1,25 +1,7 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `groups` (
-	`id_group`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`name`	TEXT,
-	`date_start`	TEXT,
-	`date_end`	TEXT
-);
-CREATE TABLE IF NOT EXISTS `impacts` (
-	`id_impact`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`id_location`	INTEGER NOT NULL,
-	`type`	TEXT DEFAULT 'loss',
-	`real`	INTEGER DEFAULT '0',
-	`felt`	INTEGER DEFAULT '0'
-);
-CREATE TABLE IF NOT EXISTS `locations` (
-	`id_location`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`name`	INTEGER
-);
-CREATE TABLE IF NOT EXISTS `projects` (
-	`id_project`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`name`	TEXT,
-	`id_group`	INTEGER NOT NULL,
-	`date_impact`	TEXT
-);
+CREATE TABLE "groups" ( `id_group`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name`	TEXT, `date_start`	TEXT, `date_end`	TEXT );
+CREATE TABLE `impacts` ( `id_impact`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `id_location`	INTEGER NOT NULL, `id_project`	INTEGER NOT NULL, `type`	TEXT DEFAULT "loss", `real`	INTEGER DEFAULT 0, `felt`	INTEGER DEFAULT 0 );
+CREATE TABLE `locations` ( `id_location`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name`	INTEGER );
+CREATE TABLE "projects" ( `id_project`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name`	TEXT, `id_group`	INTEGER NOT NULL, `date_impact`	TEXT );
+DELETE FROM "sqlite_sequence";
 COMMIT;
